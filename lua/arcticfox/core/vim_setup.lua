c1e449da7ee0171ba15fd17512399e31150e0486
Toori <bzang@gmx.de>
2024-09-30 12:54:11 +0200

    - Added autosession to save workspace setup - Fixed code - folding - fixed autocompletion

--- lua/arcticfox/core/vim_setup.lua
+++ lua/arcticfox/core/vim_setup.lua
@@ -1,6 +1,12 @@
-vim.cmd("let g:netrw_liststyle = 3")
+vim.cmd 'let g:netrw_liststyle = 3'
+
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
 local opt = vim.opt
+
+opt.foldlevel = 99 -- minimum level at which a fault is close by default, I don't want that
+opt.foldnestmax = 4 -- no more granular folds after 4 levels
 
 opt.relativenumber = true
 opt.number = true
@@ -18,12 +24,12 @@ opt.smartcase = true -- mixed case in search causes case-sensitive search
 
 opt.cursorline = true
 opt.termguicolors = true
-opt.background = "dark"
-opt.signcolumn = "yes"
+opt.background = 'dark'
+opt.signcolumn = 'yes'
 
-opt.backspace = "indent,eol,start"
+opt.backspace = 'indent,eol,start'
 
-opt.clipboard:append("unnamedplus") -- system clipboard as default register
+opt.clipboard:append 'unnamedplus' -- system clipboard as default register
 
 opt.splitright = true
 opt.splitbelow = true
